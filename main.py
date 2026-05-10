ec6f127dda05358c0eaa4bfc0b1a3bb0ca9c81cf
martinSarraciniOntivero <martinsarraciniontivero@gmail.com>
2026-05-10 16:11:51 -0300

    problema solucionado, primeras pruebas positivas y buena estructura de diseño

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 import requests
 
 ciudad = input("Ingresá una ciudad: ")
-print(ciudad)
-
-api_key = "8b279de45d4431474c0678b930456ef0"
-url = f"https://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}&units=metric"
 
 
-respuesta = requests.get(url)
+def obtener_clima(ciudad):
+    api_key = "8b279de45d4431474c0678b930456ef0"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}&units=metric"
 
-datos = respuesta.json()
+    respuesta = requests.get(url)
+    datos = respuesta.json()
+    return datos
+datosClima = obtener_clima(ciudad)
 
-print(datos)
+def traducir_clima(clima):
+    traducciones = {
+    "clear sky": "cielo despejado",
+    "few clouds": "pocas nubes",
+    "scattered clouds": "nubes dispersas",
+    "broken clouds": "nubes rotas",
+    "shower rain": "lluvia de ducha",
+    "rain": "lluvia",
+    "thunderstorm": "tormenta eléctrica",
+    "snow": "nieve",
+    "mist": "niebla",
+    "overcast clouds": "Muy nublado",
+    }
+    return traducciones.get(clima, clima)
+
+
+def mostrar_clima(datosClima):
+    climaIngles = datosClima["weather"][0]["description"]
+    climaEspañol = traducir_clima(climaIngles)
+
+    print("ciudad:", datosClima["name"])
+    print("temperatura:", datosClima["main"]["temp"], "°C")
+    print("humedad:", datosClima["main"]["humidity"], "%")
+    print("descripción:", climaEspañol)
+
+mostrar_clima(datosClima)
